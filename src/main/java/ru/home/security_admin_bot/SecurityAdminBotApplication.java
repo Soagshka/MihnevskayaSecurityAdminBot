5f138de5bf2e59e2bb224d11949fffbb86cb59e5
Soagshka <40063763+Soagshka@users.noreply.github.com>
2020-09-18 16:20:33 +0300

    First version v0.2

--- src/main/java/ru/home/security_admin_bot/SecurityAdminBotApplication.java
+++ src/main/java/ru/home/security_admin_bot/SecurityAdminBotApplication.java
@@ -2,12 +2,9 @@ package ru.home.security_admin_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.telegram.telegrambots.ApiContextInitializer;
-import ru.home.security_admin_bot.config.BotConfig;
 
 @SpringBootApplication
-@EnableConfigurationProperties(BotConfig.class)
 public class SecurityAdminBotApplication {
 
     public static void main(String[] args) {
