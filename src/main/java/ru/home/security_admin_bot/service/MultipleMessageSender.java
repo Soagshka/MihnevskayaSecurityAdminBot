516b128cfc1a2e779c1e4af34809c791c3b0f8be
Soagshka <40063763+Soagshka@users.noreply.github.com>
2020-09-18 19:38:54 +0300

    First stable version v0.6. Changed to encoding x2

--- src/main/java/ru/home/security_admin_bot/service/MultipleMessageSender.java
+++ src/main/java/ru/home/security_admin_bot/service/MultipleMessageSender.java
@@ -2,14 +2,13 @@ package ru.home.security_admin_bot.service;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 import ru.home.security_admin_bot.controller.to.RecordData;
 import ru.home.security_admin_bot.dao.UserEntity;
 import ru.home.security_admin_bot.dao.repository.UserEntityRepository;
 
+import java.net.URI;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -35,11 +34,20 @@ public class MultipleMessageSender {
                 String text = "Новая заявка  \n----------------------------------------\n Номер квартиры: " + recordData.getFlatNumber() + "\n Номер телефона: " + recordData.getPhoneNumber().replaceAll("\\+", "")
                         + "\n Марка автомобиля: " + recordData.getCarMark() + "\n Номер телефона: ";
                 RestTemplate restTemplate = new RestTemplate();
-                UriComponentsBuilder telegramRequestBuilder = UriComponentsBuilder.fromHttpUrl("https://api.telegram.org/bot" + apiToken + "/sendMessage")
-                        .queryParam("chat_id", chatId)
-                        .queryParam("text", text);
-                ResponseEntity<String> response
-                        = restTemplate.getForEntity(telegramRequestBuilder.toUriString(), String.class);
+                StringBuilder builder = new StringBuilder("http://");
+                builder.append("https://api.telegram.org/bot" + apiToken + "/sendMessage");
+                builder.append("?chat_id=");
+                builder.append(chatId);
+                builder.append("?text=");
+                builder.append(text);
+
+                URI uri = URI.create(builder.toString());
+                restTemplate.getForObject(uri, Object.class);
+//                UriComponentsBuilder telegramRequestBuilder = UriComponentsBuilder.fromHttpUrl("https://api.telegram.org/bot" + apiToken + "/sendMessage")
+//                        .queryParam("chat_id", chatId)
+//                        .queryParam("text", text);
+//                ResponseEntity<String> response
+//                        = restTemplate.getForEntity(telegramRequestBuilder.toUriString(), String.class);
 //                log.warn("urlString = " + urlString);
 //                urlString = String.format(urlString, apiToken, chatId, text);
 //
